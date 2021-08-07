364122e971703a61fc779d957899b94da80c488c
Silent <hamidreza.neo@gmail.com>
2021-08-07 14:53:30 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Mail Tm",
+    name="MailTm",
     version="0.0.1",
     author="MainSilent",
     description="Temporary Email",
