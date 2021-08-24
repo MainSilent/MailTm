65b31d15424dd5fff45fa468449c2260c2cdd9d8
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-08-24 22:45:29 +0430

    Update email.py

--- src/mailtm/email.py
+++ src/mailtm/email.py
@@ -8,7 +8,7 @@ def username_gen(length=24, chars= string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(length))  
 
 def password_gen(length=8, chars= string.ascii_letters + string.digits + string.punctuation):
-        return ''.join(random.choice(chars) for _ in range(length))  
+    return ''.join(random.choice(chars) for _ in range(length))  
 
 class Email(Listen):
     token = ""
